b3fdb9176cdb12352931bcbf2fa3d463ba1d520e
vini <velkhaliliborsato@gmail.com>
2025-05-14 21:07:40 -0300

    feat(wheatFarm)

--- main.lua
+++ main.lua
@@ -1,2 +1,66 @@
-turtle.refuel()
-turtle.forward()
+-- wget run https://raw.githubusercontent.com/vinicius-el-khalili/computercraft/refs/heads/master/main.lua
+
+local n = 9
+
+local function organizeInventory()
+    for slot=1,16,1 do
+        if(turtle.getItemDetail(slot).name=="minecraft:wheat_seeds") then
+            turtle.select(slot)
+            turtle.transferTo(1)
+        end
+    end
+    turtle.select(1)
+end
+
+local function verifySeedCount()
+    if(turtle.getItemCount(1)==0) then
+        organizeInventory()
+    end
+end
+
+local function moveForwardLine(number)
+    for i=1,number,1 do
+        turtle.forward()
+    end
+end
+
+
+local function farmBlock()
+    verifySeedCount()
+    turtle.digDown()
+    turtle.placeDown()
+end
+
+local function farmLine()
+    for i=1,n,1 do
+        farmBlock()
+        turtle.forward()
+    end
+end
+
+local function isOdd(number)
+    return number%2==1
+end
+
+local function main()
+    for i=1,n,1 do
+        farmLine()
+        if(isOdd(i)) then
+            turtle.turnLeft()
+            turtle.forward()
+            turtle.turnLeft()
+        else
+            turtle.turnRight()
+            turtle.forward()
+            turtle.turnRight()
+        end
+    end
+    turtle.turnRight()
+    moveForwardLine(n-1)
+    turtle.turnRight()
+    moveForwardLine(n-1)
+    turtle.turnRight()
+    turtle.turnRight()
+end
+
+main()
